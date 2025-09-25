f3bf3016feb5c54784ab5a34f1eaf4d117ab4843
jjflynn83 <78383171+jjflynn83@users.noreply.github.com>
2025-09-25 14:47:37 -0500

    Auto-push: update on 2025-09-25_14:47:37

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 JimBo's Finance Page")
+st.title("💰💰💰 JimBo's Finance Page 💰💰💰")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
