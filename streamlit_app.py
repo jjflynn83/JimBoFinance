73f0646a529b60bdb63280976ab7cfc4839dad94
jjflynn83 <78383171+jjflynn83@users.noreply.github.com>
2025-09-25 20:55:02 -0500

    Auto-push: update on 2025-09-25_20:55:02

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import subprocess
 from datetime import datetime, timedelta
 import streamlit.components.v1 as components
-from streamlit_js_eval import streamlit_js_eval
+import streamlit_js_eval
 
 
 
